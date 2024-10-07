dd40c46fe43f542ff0693d0fc55a24f868b9e773
Lavínia Lima <llimafreitas@icloud.com>
2024-10-07 10:58:43 -0300

    Initial commit

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.colormyviews.ui.theme.ColorMyViewsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+      setContentView(R.layout.activity_main)
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(findViewById(R.id.box_one_value), findViewById(R.id.box_two_text),
+            findViewById(R.id.box_three_text), findViewById(R.id.box_four_text), findViewById(R.id.box_five_text),
+            findViewById(R.id.constraint_layout),
+            findViewById(R.id.red_button), findViewById(R.id.yellow_button), findViewById(R.id.green_button))
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+
+    }
+    private fun makeColored(view: View) {
+        when (view.id) {
+            // Boxes using Color class colors for background
+            R.id.box_one_value -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> findViewById<View>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<View>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<View>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ColorMyViewsTheme {
+        Greeting("Android")
+    }
+}
