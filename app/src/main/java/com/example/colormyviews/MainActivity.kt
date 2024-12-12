2880487fa263dfc2097e6732d1b7514a850238e1
lavfreits <llimafreitas@icloud.com>
2024-12-12 11:43:03 -0300

    last

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,37 +4,36 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.colormyviews.ui.theme.ColorMyViewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-      setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         setListeners()
 
     }
 
     private fun setListeners() {
-        val clickableViews: List<View> = listOf(findViewById(R.id.box_one_value), findViewById(R.id.box_two_text),
-            findViewById(R.id.box_three_text), findViewById(R.id.box_four_text), findViewById(R.id.box_five_text),
+        val clickableViews: List<View> = listOf(
+            findViewById(R.id.box_one_value),
+            findViewById(R.id.box_two_text),
+            findViewById(R.id.box_three_text),
+            findViewById(R.id.box_four_text),
+            findViewById(R.id.box_five_text),
             findViewById(R.id.constraint_layout),
-            findViewById(R.id.red_button), findViewById(R.id.yellow_button), findViewById(R.id.green_button))
+            findViewById(R.id.red_button),
+            findViewById(R.id.yellow_button),
+            findViewById(R.id.green_button)
+        )
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
 
     }
+
     private fun makeColored(view: View) {
         when (view.id) {
             // Boxes using Color class colors for background
@@ -55,20 +54,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ColorMyViewsTheme {
-        Greeting("Android")
-    }
 }
